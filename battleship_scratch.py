b2d27f271d556c8f1194ea4215e4dcf7e87b27c7
ektogamut <regrant@gmail.com>
2014-02-04 22:01:15 -0600

    Separated ship placement from ship seeding.  Rudimentary print board in place.

--- battleship_scratch.py
+++ battleship_scratch.py
@@ -4,9 +4,9 @@ from random import choice
 # as with the original example because calling to one member of a list alters everything in that lists row.
 def place_board(edge):
     board = []
-    for i in range(0,edge):
+    for i in range(0, edge):
         board.append([{'hit': False}])
-        for j in range(0, edge-1):
+        for j in range(0, edge - 1):
             board[i].append({'hit': False})
     return board
 
@@ -47,7 +47,6 @@ def random_row(board):
 def random_col(board):
     return randint(0, len(board[0]) - 1)
 
-ship_loc = {}
 
 #propagates from an already defined origin
 def generate_ship(ship):
@@ -57,11 +56,11 @@ def generate_ship(ship):
     prop = ship['propagate']
     if ship['vertical']:
         for position in range(0, length):
-            inc_x = x + position*prop
+            inc_x = x + position * prop
             board[inc_x][y]['ship'] = ship['name']
     else:
         for position in range(0, length):
-            inc_y = y + position*prop
+            inc_y = y + position * prop
             board[x][inc_y]['ship'] = ship['name']
 
 
@@ -71,12 +70,12 @@ def in_sea(ship):
     length = ship['length'] - 1
     prop = ship['propagate']
     if ship['vertical']:
-        if (x + length*prop < 0) or (x + length*prop > len(board) - 1):
+        if (x + length * prop < 0) or (x + length * prop > len(board) - 1):
             return False
         else:
             return True
     else:
-        if (y + length*prop < 0) or (y + length*prop > len(board[0]) - 1):
+        if (y + length * prop < 0) or (y + length * prop > len(board[0]) - 1):
             return False
         else:
             return True
@@ -85,19 +84,19 @@ def in_sea(ship):
 def ship_collide(ship):
     x = ship['origin'][0]
     y = ship['origin'][1]
-    length = ship['length'] - 1
+    length = ship['length']
     prop = ship['propagate']
     if ship['vertical']:
-        for position in range(1, length):
-            inc_x = x + position*prop
-            if 'ship' in board[inc_x][y]: # and board[inc_x][y]['ship'] != ship['name']:
+        for position in range(0, length):
+            inc_x = x + position * prop
+            if 'ship' in board[inc_x][y] and board[inc_x][y]['ship'] != ship['name']:
                 return True
         else:
             return False
     else:
-        for position in range(1, length):
-            inc_y = y + position*prop
-            if 'ship' in board[x][inc_y]: # and board[x][inc_y]['ship'] != ship['name']:
+        for position in range(0, length):
+            inc_y = y + position * prop
+            if 'ship' in board[x][inc_y] and board[x][inc_y]['ship'] != ship['name']:
                 return True
         else:
             return False
@@ -109,35 +108,56 @@ def board_position(ship):
     return board[x][y]
 
 
-def place_ship(ship):
+def is_real(ship):
+    if in_sea(ship) is True:
+        if ship_collide(ship) is True:
+            return False
+        else:
+            return True
+    else:
+        return False
+
+
 def seed_ship(ship):
     if ship in ships:
         ship['origin'] = [random_row(board), random_col(board)]
         ship['vertical'] = choice([True, False])
         ship['propagate'] = choice([-1, 1])
-        while not in_sea(ship) and ship_collide(ship):
-            ship['origin'] = [random_row(board), random_col(board)]
+
+
+def place_ship(ship):
+    seed_ship(ship)
+    while is_real(ship) is False:
+        seed_ship(ship)
+    else:
         generate_ship(ship)
 
 
-
-
-place_ship(destroyer)
 place_ship(battleship)
-print "destroyer info: ", destroyer
+place_ship(destroyer)
+place_ship(patrol)
 print "battleship info: ", battleship
+print "destroyer info: ", destroyer
+print in_sea(destroyer), ship_collide(destroyer), is_real(destroyer)
+
 for row in board:
     print row
 
 
-#
-# generate_ship(battleship)
-# print "after generate_ship"
-# for row in board:
-#     print row
-#
-# if 'battleship'in board:
-#     print board_position(battleship)
-# # print board[destroyer['origin'][0]]
-#
+def print_board(board):
+    board_output = []
+    for i in range(0, len(board)):
+        board_output.append([" "])
+        for j in range(0, len(board[0])):
+            board_output[i].append(" ")
+            if 'ship' in board[i][j]:
+                # print i, j, board[i][j]['ship']
+                board_output[i][j] = board[i][j]['ship'][0]
+            elif board[i][j]['hit'] == False:
+                board_output[i][j] = "X"
+    for row in board_output:
+        print " ".join(row)
 
+
+
+print_board(board)
